d0b89a8f08eba6ea828dbcb7d67483bcea179002
Shintaro Morikawa <sntr92@gmail.com>
2015-06-06 19:54:22 +0900

    Save post info to DB

--- server.rb
+++ server.rb
@@ -3,6 +3,7 @@ require 'rack'
 require 'digest/md5'
 require 'sdbm'
 require 'dotenv'
+require 'mysql2-cs-bind'
 
 module Gyazo
   class Controller < Sinatra::Base
@@ -10,8 +11,19 @@ module Gyazo
 
     configure do
       set :image_dir, 'public'
-      set :image_url, 'http://g.morishin.me'
+      set :image_url, ENV['SERVER_URL']
       set :access_token, ENV['ACCESS_TOKEN']
+    end
+
+    helpers do
+      def db
+        Mysql2::Client.new(
+          :host => ENV['DB_HOST'],
+          :username => ENV['DB_USER'],
+          :password => ENV['DB_PASS'],
+          :database => ENV['DB_NAME']
+        )
+      end
     end
 
     post '/' do
@@ -19,10 +31,13 @@ module Gyazo
       if auth != "Bearer " + options.access_token then
         return status 403
       end
+
       id = request[:id]
       data = request[:imagedata][:tempfile].read
       hash = Digest::MD5.hexdigest(data).to_s
       File.open("#{options.image_dir}/#{hash}.png", 'w'){|f| f.write(data)}
+
+      db.xquery("INSERT INTO images (`hash`, `created_at`) VALUES (?, ?)", hash, Time.now)
 
       "#{options.image_url}/#{hash}.png"
     end
