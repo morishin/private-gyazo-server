3ac3714cdb1832cd9cd8cda672caf2a33b2ef833
Shintaro Morikawa <sntr92@gmail.com>
2015-06-19 17:34:32 +0900

    Basic auth

--- server.rb
+++ server.rb
@@ -25,7 +25,23 @@ module Gyazo
           :database => ENV['DB_NAME']
         )
       end
+
+      def protect!
+        unless authorized?
+          response['WWW-Authenticate'] = %(Basic realm="Restricted Area")
+          throw(:halt, [401, "Not authorized\n"])
+        end
+      end
+
+      def authorized?
+        @auth ||=  Rack::Auth::Basic::Request.new(request.env)
+        username = ENV['BASIC_AUTH_USERNAME']
+        password = ENV['BASIC_AUTH_PASSWORD']
+        @auth.provided? && @auth.basic? && @auth.credentials && @auth.credentials == [username, password]
+      end
     end
+
+    before { protect! if request.path_info == "/history" }
 
     post '/upload' do
       auth = request.env['HTTP_AUTHORIZATION']
